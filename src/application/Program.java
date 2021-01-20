65d6ba4708828650c1456d08d4fd01ce8430423f
Gislaine <gimcjf@gmail.com>
2021-01-20 17:19:20 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package application;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.etities.Department;
 import model.etities.Seller;
 
@@ -22,6 +19,8 @@ public class Program {
         
         Department obj = new Department(1, "Books");
         Seller s = new Seller(21, "Bob", "bob@gmil.com", new java.sql.Date(sdf.parse("06/06/1990").getTime()), 3000.0, obj);
+        SellerDao sellerDao = DaoFactory.createSellerDao();
+        
         System.out.println(s);
     }
     
